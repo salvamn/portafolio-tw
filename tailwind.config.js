37c244274a75df3d202096fc1ab4225e798f9b90
Salvador Arnaldo Mellado Nuñez <salvador0796@gmail.com>
2024-03-02 23:05:53 -0300

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'quick': ["Quicksand", "sans - serif"],
+    }
+  },
+  plugins: [],
+}
+
